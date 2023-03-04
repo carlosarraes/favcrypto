eaa13f58dfe13cea591c9890b5754a858451f262
carraes <carraeshb@gmail.com>
2023-03-04 02:01:38 -0300

    fix: postgres table creation

--- init.sql
+++ init.sql
@@ -5,19 +5,19 @@ CREATE TABLE Currency.data(
     favorite BOOLEAN NOT NULL,
     name VARCHAR(20) NOT NULL,
     symbol VARCHAR(4) NOT NULL,
-    price DECIMAL(10,2)
+    price REAL NOT NULL DEFAULT 0
 );
 
 INSERT INTO Currency.data
   (favorite, name, symbol, price)
 VALUES
-  (TRUE, 'Klever', 'KLV', NULL),
-  (FALSE, 'Bitcoin', 'BTC', NULL),
-  (FALSE, 'Ethereum', 'ETH', NULL),
-  (FALSE, 'Enjin', 'ENJ', NULL),
-  (FALSE, 'Binance', 'BNB', NULL),
-  (FALSE, 'Litecoin', 'LTC', NULL),
-  (FALSE, 'Apecoin', 'APE', NULL),
-  (FALSE, 'Chainlink', 'LINK', NULL),
-  (FALSE, 'Dogecoin', 'DOGE', NULL),
-  (FALSE, 'TRON', 'TRX', NULL);
+  (TRUE, 'Klever', 'KLV', 0),
+  (FALSE, 'Bitcoin', 'BTC', 0),
+  (FALSE, 'Ethereum', 'ETH', 0),
+  (FALSE, 'Enjin', 'ENJ', 0),
+  (FALSE, 'Binance', 'BNB', 0),
+  (FALSE, 'Litecoin', 'LTC', 0),
+  (FALSE, 'Apecoin', 'APE', 0),
+  (FALSE, 'Chainlink', 'LINK', 0),
+  (FALSE, 'Dogecoin', 'DOGE', 0),
+  (FALSE, 'TRON', 'TRX', 0);
