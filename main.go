94ee69e38007679d51675efea89445faa20ca6d5
carraes <carraeshb@gmail.com>
2023-03-04 10:05:09 -0300

    feat: testing basic server func

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"favcrypto/data"
+	"favcrypto/handlers"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func init() {
@@ -10,4 +14,10 @@ func init() {
 }
 
 func main() {
+	http.HandleFunc("/", handlers.HandleRequest)
+	fmt.Println("Server is running on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %q", err)
+	}
 }
