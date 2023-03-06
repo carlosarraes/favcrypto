ad5793046075925752f843cc240018bb46230da7
carraes <carraeshb@gmail.com>
2023-03-05 22:16:11 -0300

    feat: downvote for better management

--- handlers/controlvote.go
+++ handlers/controlvote.go
@@ -12,7 +12,7 @@ func (h *Handlers) HandleUpvote(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case http.MethodPatch:
-		writeHeader(w, check)
+		writeHeader(w, true, check)
 	default:
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -22,11 +22,31 @@ func (h *Handlers) HandleUpvote(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Upvoted: %q\n", strings.ToUpper(path))
 }
 
-func writeHeader(w http.ResponseWriter, c int64) {
+func (h *Handlers) HandleDownvote(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimPrefix(r.URL.Path, "/downvote/")
+	check := h.db.UpdateFavorite(false, strings.ToUpper(path))
+
+	switch r.Method {
+	case http.MethodPatch:
+		writeHeader(w, false, check)
+	default:
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, `{"message": "Method not allowed"}`)
+	}
+
+	fmt.Printf("Upvoted: %q\n", strings.ToUpper(path))
+}
+
+func writeHeader(w http.ResponseWriter, t bool, c int64) {
 	if c > 0 {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, `{"message": "Upvoted"}`)
+		s := `{"message": "Upvoted"}`
+		if !t {
+			s = `{"message": "Downvoted"}`
+		}
+		fmt.Fprint(w, s)
 	} else {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusNotFound)
